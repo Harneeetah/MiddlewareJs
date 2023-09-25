0ede2ac6434c19908d16783f8f6f29bbaf842dde
harneeetah <harneeetah@gmail.com>
2023-09-25 23:06:52 +0100

    error handling

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require('cors');
+const {logger} = require('./middleware/logEvents');
+const errorHandler = require('./middleware/errorHandler.js');
 const PORT = process.env.PORT || 3000 ;
 
+
 //creating a custom middleware for logging
-app.use((req, res, next) => {
-    console.log(`${req.method} ${req.path}`);
-    next();
-});
+app.use(logger);
+
+//Cross origin resource sharing
+const whitelist = ['https://www.google.com', 'http://127.0.0.1:5500', 'http://localhost:3000'];
+
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions));
 
 //There are built-in middleware.
 app.use(express.urlencoded({extended : false}));
@@ -63,8 +80,18 @@ app.get('/hello(.html)?', (req, res, next) => {
 
  app.get('/chain(.html)?', [one, two, three]);
 
-app.get('/*', (req, res) => {
-    res.status(404).sendFile(path.join(__dirname, 'webviews','404.html'));
+ //handles all requests to web address
+ app.all('*', (req, res) => {
+    res.status(404);
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    } else if (req.accepts('json')) {
+        res.json({ "error": "404 Not Found" });
+    } else {
+        res.type('txt').send("404 Not Found");
+    }
 });
+
+app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
